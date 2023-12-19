33d6ee79bfcaab6b4f9b8577c01a3c2ac001ab8a
emrailklmin <emrailklmin@gmail.com>
2023-12-19 12:13:49 +0100

    for render

--- Flaskcalc.py
+++ Flaskcalc.py
@@ -96,5 +96,5 @@ def calculate_gpa(courses_list):
 
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
